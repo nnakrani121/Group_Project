437bf67912d7770756f5d975353d78e804b109b9
nikhi <nnakrani121@rku.ac.in>
2020-08-05 09:17:42 +0530

    In This Project making Login Page

---
+++ app/src/main/java/com/example/loginpage/Splash_screen.java
@@ -0,0 +1,21 @@
+package com.example.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override public void run() {
+                Intent i = new Intent(Splash_screen.this, MainActivity.class); startActivity(i);
+                finish(); } }, 3000);
+    }
+}
